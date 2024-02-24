779d0c49ddf07ce8e7823220c07f6468770e442d
Daniel Huynh <danielhuynh523@gmail.com>
2024-02-24 15:29:41 -0500

    started the frontend

---
+++ clinivision-frontend/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">CliniVision</a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <header className="App-header">
+        <h1>
+          JAREN BRESNICK'S PROJECT CODE PRESENTS:
+        </h1>
+        <h1>
+          CLINIVISION
+        </h1>
+      </header>
+
+      <img class="height" src='jaren.jpg' className="App-logo" alt="logo" />
+      <h4 class="mt-5">
+        Sponsored by Rishi Sureshkumar, Pre-Med Computer Science Major at the University of Virginia.
+      </h4>
+    </div>
+  );
+}
+
+export default App;
