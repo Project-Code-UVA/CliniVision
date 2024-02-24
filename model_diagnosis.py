4057dce67577d2a0ce94e2b9fea89c214dce2788
Rishi Sureshkumar <123980270+Rishi-Sureshkumar@users.noreply.github.com>
2024-02-24 13:50:42 -0500

    Added comments on symptoms

--- model_diagnosis.py
+++ model_diagnosis.py
@@ -3,6 +3,22 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 
+# # 0 - Aortic enlargement
+# 1 - Atelectasis
+# 2 - Calcification
+# 3 - Cardiomegaly
+# 4 - Consolidation
+# 5 - ILD
+# 6 - Infiltration
+# 7 - Lung Opacity
+# 8 - Nodule/Mass
+# 9 - Other lesion
+# 10 - Pleural effusion
+# 11 - Pleural thickening
+# 12 - Pneumothorax
+# 13 - Pulmonary fibrosis
+# 14 - No finding
+
 # Load the dataset
 file_path = 'train.csv'  # Update this path with your actual file location
 data = pd.read_csv(file_path)
