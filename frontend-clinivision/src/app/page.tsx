2ecce260eb4bad3295952f40af2a3d269ac55553
Daniel Huynh <danielhuynh523@gmail.com>
2024-02-26 15:56:57 -0500

    restyled the front page

--- frontend-clinivision/src/app/page.tsx
+++ frontend-clinivision/src/app/page.tsx
@@ -1,41 +1,44 @@
 "use client";
-import './App.css';
+import React, { useState } from "react";
 import Image from "next/image";
-import React, {useState} from "react";
-import FileUpload from './FileUpload.js'
+import FileUpload from './FileUpload.js';
 import DragDrop from './DragDrop.js';
+import './App.css';
 
 export default function Home() {
-  const [file, setFile] = useState();
+  const [file, setFile] = useState('');
+
   function handleChange(e) {
-    console.log(e.target.files);
-    setFile(URL.createObjectURL(e.target.files[0]));
+    const uploadedFile = e.target.files[0];
+    const fileUrl = URL.createObjectURL(uploadedFile);
+    setFile(fileUrl);
   }
 
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <h1>
-          JAREN BRESNICK'S PROJECT CODE PRESENTS:
-        </h1>
-        <h1 className="mb-5">
-          CLINIVISION
-        </h1>
+    <div className="flex flex-col min-h-screen bg-purple-100 justify-center items-center">
+      <img src='jaren.jpg' className="App-logo mb-20" alt="logo" />
+      <header className="text-center mb-8">
+        <h1 className="text-3xl font-bold">CLINIVISION</h1>
+        <p className="text-sm text-gray-600">Presented by Jaren Bresnick's Project Code</p>
       </header>
 
-      <img src='jaren.jpg' className="App-logo" alt="logo" />
-
-      <div className="imageSubmission">
-        <h2 className="mb-4">Submit Image</h2>
-        <input type="file" onChange={handleChange}/>
-        <img src={file} />
-        {/* <FileUpload></FileUpload> */}
-        {/* <DragDrop></DragDrop> */}
+      <div className="w-96 bg-white shadow-md rounded-lg overflow-hidden">
+        <div className="px-6 py-8">
+          <h2 className="text-xl font-semibold mb-4">Submit Image</h2>
+          <label htmlFor="imageUpload" className="block w-full bg-blue-500 text-white py-2 px-4 rounded-md text-center cursor-pointer mb-4">
+            Upload Image
+          </label>
+          <input id="imageUpload" type="file" onChange={handleChange} className="hidden" />
+          {file && <img src={file} className="w-full h-auto mb-4" alt="Uploaded" />}
+          {/* Additional upload methods */}
+          {/* <FileUpload></FileUpload> */}
+          {/* <DragDrop></DragDrop> */}
+        </div>
       </div>
-      <h4 className="mt-5">
-        Sponsored by Rishi Sureshkumar, Pre-Med Computer Science Major at the University of Virginia. 
-      </h4>
+
+      <footer className="text-sm text-gray-600 mt-auto bottom-0 fixed w-full bg-white p-4">
+        Sponsored by Rishi Sureshkumar, Pre-Med Computer Science Major at the University of Virginia.
+      </footer>
     </div>
   );
 }
