b19eeb634b18a78876346908596714c1cd424a18
Daniel Huynh <danielhuynh523@gmail.com>
2024-02-28 01:04:15 -0500

    changed some spacing and coloring styles

--- frontend-clinivision/src/app/page.tsx
+++ frontend-clinivision/src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col min-h-screen">
-    <nav className="flex justify-between items-center p-4 bg-blue-500 text-white">
+    <nav className="flex justify-between items-center p-4 bg-violet-500 text-white">
       <div className="flex gap-x-4"> 
         <a href="/" className="text-lg font-semibold">Home</a>
         <a href="/about" className="text-lg font-semibold">About</a>
@@ -34,12 +34,12 @@ export default function Home() {
         
         <div className="flex-grow flex flex-col justify-center items-center bg-violet-300">
           <div className="w-full flex flex-col items-center">
-            <img src='logo.jpg' className="mb-5 w-24 h-24" alt="logo" />
+            <img src='logo.jpg' className="mb-5 mt-2 w-24 h-24" alt="logo" />
             <h1 className="text-5xl font-serif mb-2">CLINIVISION</h1>
             <p className="text-base text-gray-600">Empowering Medical Diagnostics</p>
           </div>
 
-          <div className="mt-8">
+          <div className="mt-8 mb-3">
             <div className="bg-white shadow-md rounded-lg p-6 max-w-lg">
               <h2 className="text-2xl font-semibold mb-6 text-center">Submit Your Image</h2>
               <div {...getRootProps({ className: 'dropzone border-2 border-dashed border-gray-300 p-6 mb-4 rounded-md text-center' })}>
@@ -47,7 +47,7 @@ export default function Home() {
                 {isDragActive ? <p>Drop the files here ...</p> : <p>Drag 'n' drop some files here, or click to select files</p>}
                 {preview && <img src={preview} className="w-full h-auto rounded-md mt-4" alt="Preview" />}
               </div>
-              <button className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-150 ease-in-out" disabled={!file}>
+              <button className="w-full bg-violet-500 text-white py-2 px-4 rounded-md hover:bg-violet-600 transition duration-150 ease-in-out" disabled={!file}>
                 Submit
               </button>
             </div>
