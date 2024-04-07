221a57430e4500367553646ca8761ec035a78b6c
Jaren Bresnick <Jefbresnick@gmail.com>
2024-04-07 00:19:40 -0400

    download iamges from box

--- Localization/get_box.py
+++ Localization/get_box.py
@@ -1,31 +1,29 @@
 import pandas as pd
 from boxsdk import OAuth2, Client
 import ast
+import os
 
-# Function to find and print matching files based on symptom input
-def find_matching_files_based_on_symptom(symptom):
+def find_matching_files_based_on_symptom(symptom, download_folder_path, num_images=None):
     csv_file_path = 'output.csv'
     CLIENT_ID = ''
     CLIENT_SECRET = ''
     ACCESS_TOKEN = ''
     FOLDER_ID = ''
 
-    # Read the CSV file
+    if not os.path.exists(download_folder_path):
+        os.makedirs(download_folder_path)
+
     df = pd.read_csv(csv_file_path)
     
-    # Ensure symptom is within the expected range
     if symptom < 0 or symptom > 14:
         raise ValueError("Symptom input must be between 0 and 14")
 
-    # Get the list_like_string based on symptom input
     list_like_string = df.loc[symptom, 'image_id']
     image_ids = ast.literal_eval(list_like_string)
 
-    # Setup Box SDK client
     auth = OAuth2(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, access_token=ACCESS_TOKEN)
     client = Client(auth)
 
-    # Find matching files in the specified folder
     matching_files = []
     folder_items = client.folder(folder_id=FOLDER_ID).get_items()
     for item in folder_items:
@@ -33,9 +31,17 @@ def find_matching_files_based_on_symptom(symptom):
         if file_name_without_extension in image_ids:
             matching_files.append(item)
 
-    # Print the names of matching files
-    for matching_file in matching_files:
-        print(matching_file.name)
+    # Limit the number of files to download if num_images is specified
+    files_to_download = matching_files if num_images is None else matching_files[:num_images]
 
-# Example usage
-find_matching_files_based_on_symptom(0)
+    for matching_file in files_to_download:
+        print(matching_file.name)
+        file_path = os.path.join(download_folder_path, matching_file.name)
+        with open(file_path, 'wb') as open_file:
+            matching_file.download_to(open_file)
+    
+    print(f"{len(files_to_download)} files downloaded.")
+
+
+download_folder_path = 'Aortic_Enlargement_Images'
+find_matching_files_based_on_symptom(0, download_folder_path, num_images=100)
