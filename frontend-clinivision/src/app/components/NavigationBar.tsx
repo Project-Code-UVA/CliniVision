0557b3aaac9cff563d2fb6cb907d4a0c23b127a1
Jaren Bresnick <Jefbresnick@gmail.com>
2024-04-18 15:34:16 -0400

    updated spacing on navbar

--- frontend-clinivision/src/app/components/NavigationBar.tsx
+++ frontend-clinivision/src/app/components/NavigationBar.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NavigationBar: React.FC = () => {
   return (
-    <nav className="flex justify-between items-center p-4 bg-violet-600 text-white">
+    <nav className="flex justify-between items-center p-4 bg-violet-600 text-white font-semibold">
       <div className="flex gap-x-4">
-        <a href="/" className="text-lg font-semibold">Home</a>
+        <a href="/" className="text-lg font-semibold px-4">Home</a>
         <a href="/about" className="text-lg font-semibold">About</a>
       </div>
       <a href="/login" className="text-lg font-semibold">Login</a>
