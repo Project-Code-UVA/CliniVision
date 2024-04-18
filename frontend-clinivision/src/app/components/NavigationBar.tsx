fd719ba492bc8561c88fba93884e18302c24d465
Jaren Bresnick <Jefbresnick@gmail.com>
2024-04-18 15:34:58 -0400

    updated navbar spacing

--- frontend-clinivision/src/app/components/NavigationBar.tsx
+++ frontend-clinivision/src/app/components/NavigationBar.tsx
@@ -7,7 +7,7 @@ const NavigationBar: React.FC = () => {
         <a href="/" className="text-lg font-semibold px-4">Home</a>
         <a href="/about" className="text-lg font-semibold">About</a>
       </div>
-      <a href="/login" className="text-lg font-semibold">Login</a>
+      <a href="/login" className="text-lg font-semibold px-4">Login</a>
     </nav>
   );
 };
